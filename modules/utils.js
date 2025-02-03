40115d56bfbaf6d769f3bb83e091d86dbf61507e
John Guo <65568323+Snowballxld@users.noreply.github.com>
2025-02-02 22:15:08 -0800

    Split part b and c into 2 different modules

--- modules/utils.js
+++ modules/utils.js
@@ -1,20 +1,8 @@
-const fs = require("fs");
-const path = require("path");
+//Part B
+class DateHandler {
+    getDate() {
+        return new Date().toString();
+    }
+}
 
-//for part B
-module.exports.getDate = function () {
-    return new Date().toString();
-};
-
-//for part C
-const filePath = path.join(__dirname, "../file.txt");
-
-//writing
-module.exports.appendToFile = function (text, callback) {
-    fs.appendFile(filePath, text + "\n", () => callback("Text appended successfully!"));
-};
-
-//reading
-module.exports.readFile = function (callback) {
-    fs.readFile(filePath, "utf8", (_, data) => callback(data));
-};
+module.exports = DateHandler;
