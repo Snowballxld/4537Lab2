3dcf65ff6bb70eb4c4b91fa84661de20a85c180b
John Guo <65568323+Snowballxld@users.noreply.github.com>
2025-02-02 21:56:32 -0800

    Update utils.js
    Added Part C

--- modules/utils.js
+++ modules/utils.js
@@ -1,3 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+//for part B
 module.exports.getDate = function () {
     return new Date().toString();
+};
+
+//for part C
+const filePath = path.join(__dirname, "../file.txt");
+
+//writing
+module.exports.appendToFile = function (text, callback) {
+    fs.appendFile(filePath, text + "\n", () => callback("Text appended successfully!"));
+};
+
+//reading
+module.exports.readFile = function (callback) {
+    fs.readFile(filePath, "utf8", (_, data) => callback(data));
 };
